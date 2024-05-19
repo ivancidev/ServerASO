a68e64d90626df70d7189dd40df9c46db08a6ab3
Herlan <herlanherbas@gmail.com>
2024-05-19 18:45:53 -0400

    implemented function get_shares

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from src.routes import sambaRoute
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/', methods=['GET'])
 def index():
     return sambaRoute.greet()
 
-@app.route('/shares')
-def index():
+@app.route('/shares', methods=['GET'])
+def shares():
     return sambaRoute.get_shares()
-
 
 
 if __name__ == "__main__":
