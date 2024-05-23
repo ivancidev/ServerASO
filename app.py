f49af78bf66aec364065e0a19cd8d24d91b6709b
Herlan <herlanherbas@gmail.com>
2024-05-22 20:03:04 -0400

    add: route rename_share

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from src.routes import sambaRoute
 from flask_cors import CORS
 
@@ -11,6 +11,15 @@ def index():
 @app.route('/shares', methods=['GET'])
 def shares():
     return sambaRoute.get_shares()
+
+@app.route('/renameShare', methods=['POST'])
+def rename_share_route():
+    data = request.json
+    old_name = data['oldName']
+    new_name = data['newName']
+    sambaRoute.rename_share(old_name, new_name)
+
+    return jsonify({'message': 'Share renamed succesfully'})
 
 
 if __name__ == "__main__":
